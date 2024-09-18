c3e74e827fa3619f1ef85f18a7cb1e265ad8f173
vitor <valtervitorfilho@gmail.com>
2024-09-18 20:41:51 -0300

    att v18-09-2024

---
+++ src/main/java/com/api_vendinha/api/domain/entities/Produto.java
@@ -0,0 +1,37 @@
+package com.api_vendinha.api.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Entidade representando um produto no sistema.
+ */
+@Entity
+@Table(name = "produtos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Renomeado para seguir a convenção Java
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String quantidade;
+
+    @Column(nullable = false)
+    private String preco;
+
+    @Column
+    private Boolean isActive; // Renomeado para seguir a convenção Java
+
+    @ManyToOne
+    @JoinColumn( name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/api_vendinha/api/domain/service/UserServiceImpl.java
@@ -0,0 +1,124 @@
+package com.api_vendinha.api.domain.service;
+
+import com.api_vendinha.api.Infrastructure.repository.UserRepository;
+import com.api_vendinha.api.domain.dtos.request.UserRequestDto;
+import com.api_vendinha.api.domain.dtos.response.UserResponseDto;
+import com.api_vendinha.api.domain.entities.Produto;
+import com.api_vendinha.api.domain.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.api_vendinha.api.infrastructure.repository;
+
+
+
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserServiceInterface {
+
+    // Repositório para a persistência de dados de usuários.
+    private final UserRepository userRepository;
+    private  final com.api_vendinha.api.infrastructure.repository.ProdutoRepository
+
+    /**
+     * Construtor para injeção de dependência do UserRepository.
+     *
+     * @param userRepository O repositório de usuários a ser injetado.
+     */
+    @Autowired
+    public UserServiceImpl(com.api_vendinha.api.Infrastructure.repository.UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Salva um novo usuário ou atualiza um usuário existente.
+     * <p>
+     * Cria uma nova entidade User a partir dos dados fornecidos no UserRequestDto, persiste essa
+     * entidade no banco de dados, e retorna um UserResponseDto com as informações do usuário salvo.
+     *
+     * @param userRequestDto DTO contendo os dados do usuário a ser salvo ou atualizado.
+     * @return DTO com as informações do usuário salvo, incluindo o ID gerado e o nome.
+     */
+    @Override
+    public UserResponseDto save(UserRequestDto userRequestDto) {
+        // Cria uma nova instância de User.
+        User user = new User();
+        // Define o nome do usuário a partir do DTO.
+        user.setName(userRequestDto.getName());
+        user.setEmail(userRequestDto.getEmail());
+        user.setPassword(userRequestDto.getPassword());
+        user.setCep(userRequestDto.getCep());
+        user.setCpf(userRequestDto.getCpf());
+        user.setIs_active(Boolean.TRUE);
+
+
+        // Salva o usuário no banco de dados e obtém a entidade persistida com o ID gerado.
+        User savedUser = userRepository.save(user);
+
+        List<Produto> produtos= userRequestDto.getProdutoRequestDtos().stream().map(dto-> {
+
+                    Produto produto = new Produto();
+                    Produto.setUser(savedUser);
+                    Produto.setQuantidade(dto.getQuantidade());
+                    Produto.setName(dto.getName());
+                    Produto.setPreco(dto.getPreco());
+                    Produto.setIs_active(dto.getIs_active());
+                    return produto;
+
+        });
+
+
+
+        // Retorna o DTO com as informações do usuário salvo.
+        return this.getUserResponseDto(user);
+    }
+
+    @Override
+    public UserResponseDto update(Long id, UserRequestDto userRequestDto) {
+        User user = userRepository.findById(id).orElseThrow();
+
+        user.setName(userRequestDto.getName());
+        user.setEmail(userRequestDto.getEmail());
+        user.setPassword(userRequestDto.getPassword());
+        user.setCep(userRequestDto.getCep());
+        user.setCpf(userRequestDto.getCpf());
+        user.setIs_active(Boolean.TRUE);
+
+        userRepository.save(user);
+
+        return getUserResponseDto(user);
+    }
+
+
+    @Override
+    public UserResponseDto getId(Long id) {
+        User user = userRepository.findById(id).orElseThrow();
+
+        return getUserResponseDto(user);
+    }
+
+    @Override
+    public UserResponseDto setActive(long id, UserRequestDto is_active) {
+        User user = userRepository.findById(id).orElseThrow();
+
+        user.setIs_active(is_active.getIs_active());
+        userRepository.save(user);
+
+        return getUserResponseDto(user);
+    }
+
+    private UserResponseDto getUserResponseDto(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+
+        userResponseDto.setId(user.getId());
+        userResponseDto.setName(user.getName());
+        userResponseDto.setEmail(user.getEmail());
+        userResponseDto.setPassword(user.getPassword());
+        userResponseDto.setCep(user.getCep());
+        userResponseDto.setCpf(user.getCpf());
+        userResponseDto.setIs_active(user.getIs_active());
+
+        return userResponseDto;
+    }
+}
